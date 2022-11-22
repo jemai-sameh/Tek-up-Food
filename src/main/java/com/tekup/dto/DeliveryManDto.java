8e7879b534fbe553eac5d16e985f221cd809670e
Ferdaws <ferdawsksiksi2015@gmail.com>
2022-11-22 18:48:08 +0100

    add function fromListEntity in DeliveryManDto

--- src/main/java/com/tekup/dto/DeliveryManDto.java
+++ src/main/java/com/tekup/dto/DeliveryManDto.java
@@ -1,5 +1,13 @@
 package com.tekup.dto;
 
+import com.tekup.model.DeliveryMan;
+
+import java.util.List;
+
 public class DeliveryManDto {
 
+
+    public static List<DeliveryManDto> fromListEntity(List<DeliveryMan> list) {
+        return null;//list.stream().map(x -> fromEntity(x)).collect(Collectors.toList());
+    }
 }
